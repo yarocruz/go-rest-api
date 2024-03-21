1f9a6fad7b6486cf394cc97a52c64afcf6c0be59
yarocruz <yarocruz@gmail.com>
2024-03-21 13:00:57 -0400

    add save to DB biz-logic

--- db/db.go
+++ db/db.go
@@ -9,7 +9,8 @@ import (
 var DB *sql.DB
 
 func InitDB() {
-	DB, err := sql.Open("sqlite3", "api.db") // api gets created automatically if it doesn't exist
+	var err error
+	DB, err = sql.Open("sqlite3", "api.db") // api gets created automatically if it doesn't exist
 	
 	if err != nil {
 		panic("Could not connect to database.")
@@ -17,4 +18,25 @@ func InitDB() {
 
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
+
+	createTables()
+}
+
+func createTables() {
+	createEventsTable := `
+	CREATE TABLE IF NOT EXISTS events (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		dateTime DATETIME NOT NULL,
+		user_id INTEGER
+	)
+	`
+
+	_, err := DB.Exec(createEventsTable)
+
+	if err != nil {
+		panic("Could not create events table.")
+	}
 }
