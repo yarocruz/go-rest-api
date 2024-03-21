1f072ac8cc4a0a908e464a1c5db9a15bc2d98ac0
yarocruz <yarocruz@gmail.com>
2024-03-21 17:53:18 -0400

    making data persistent

--- models/event.go
+++ models/event.go
@@ -40,6 +40,29 @@ func (e Event) Save() error {
 	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := `
+		SELECT * FROM events
+	`
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+
+	}
+
+	return events, nil
 }
