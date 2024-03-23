85ea735b90753fe85e1c97e0216f4c1b1afdc5a3
yarocruz <yarocruz@gmail.com>
2024-03-23 11:16:30 -0400

    separate routes

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"net/http"
-
 	"example.com/rest-api/db"
-	"example.com/rest-api/models"
+	"example.com/rest-api/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,39 +10,9 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
 
 	server.Run(":8080") // localhost:8080
 }
 
-func getEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
 
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message" : "Could not fetch events. Try again later"})
-	}
-	context.JSON(http.StatusOK, events)
-}
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 1
-
-	err = event.Save()
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not create event. Please try again later"})
-		return
-	}
-
-	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
-}
