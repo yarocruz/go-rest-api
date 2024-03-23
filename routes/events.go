a397a1540f42bbe7457a4353e9d93e920ce53a9d
yarocruz <yarocruz@gmail.com>
2024-03-23 11:49:04 -0400

    add update event route

--- routes/events.go
+++ routes/events.go
@@ -56,3 +56,35 @@ func createEvent(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id"})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+	
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not find the event"})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+	
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not uddate event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated succesfully"})
+}
