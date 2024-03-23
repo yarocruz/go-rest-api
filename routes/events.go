d0d0fa0848929f8b6a1b4dcd0d9e9e10a54d0ed3
yarocruz <yarocruz@gmail.com>
2024-03-23 11:59:59 -0400

    added DELETE event route

--- routes/events.go
+++ routes/events.go
@@ -88,3 +88,28 @@ func updateEvent(context *gin.Context) {
 
 	context.JSON(http.StatusOK, gin.H{"message": "Event updated succesfully"})
 }
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+	
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not find the event"})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not delete the event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted succesfully"})
+
+}
