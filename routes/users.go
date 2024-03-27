a7f9d92a80cc28123be56c4f5e34577b3ec03e70
yarocruz <yarocruz@gmail.com>
2024-03-27 14:23:56 -0400

    add login route

--- routes/users.go
+++ routes/users.go
@@ -26,3 +26,23 @@ func signup(context *gin.Context) {
 	
 	context.JSON(http.StatusCreated, gin.H{"message": "User created succesfully"})
 }
+
+func login(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	err = user.ValidateUser()
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not authenticate user"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+}
