4322cc6e1b42935f1140c6e78df8a9273fb48c44
yarocruz <yarocruz@gmail.com>
2024-03-28 11:21:30 -0400

    event route is protected

--- models/user.go
+++ models/user.go
@@ -41,7 +41,7 @@ func (u User) Save() error {
 	return err
 }
 
-func (u User) ValidateUser() error{
+func (u *User) ValidateUser() error{
 	query := "SELECT id, password FROM users WHERE email = ?"
 	row := db.DB.QueryRow(query, u.Email)
 	
--- routes/events.go
+++ routes/events.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"example.com/rest-api/models"
+	"example.com/rest-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -43,16 +44,22 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
 		return
 	}
 
-	event.ID = 1
-	event.UserID = 1
+	event.UserID = userId
 
 	err = event.Save()
 
