856e7189b9a510528f4677984faab1abacab1791
yarocruz <yarocruz@gmail.com>
2024-03-28 10:34:56 -0400

    add jwt util

---
+++ utils/jwt.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const secretKey = "supersecret"
+
+func GenereateToken(email string, userId int64) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": email,
+		"userId": userId,
+		"exp": time.Now().Add(time.Hour * 2).Unix(), // expires after two hours
+	})
+
+	return token.SignedString(secretKey)
+}
