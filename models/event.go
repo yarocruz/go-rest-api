efa03503a100dac8bbfeab377df3b5d531308bdf
yarocruz <yarocruz@gmail.com>
2024-03-29 11:51:14 -0400

    add register for event routes

--- models/event.go
+++ models/event.go
@@ -116,3 +116,19 @@ func (event Event) Delete() error {
 	return err
 
 }
+
+func (e Event) Register(userId int64) error {
+	query := "INSERT INTO registration(event_id, user_id) VALUES (?,?)"
+
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(e.ID, userId)
+
+	return err
+}
